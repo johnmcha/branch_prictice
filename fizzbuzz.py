d066aac9527638f80184bb8632a1413f13f41d48
johnmcha <johnnycha90@gmail.com>
2023-02-03 14:31:09 +0900

    feat: add one joint condition that return 'fizzbuzz' when i is a multiple of 15

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,10 +5,12 @@ print(user_num)
 
 #iteration
 for i in range(1, user_num):
-    if i%3 == 0:
-        print('fizz')
-    if i%5 == 0:
+    if i% 3 ==0 and i%5 == 0:
+        print('fizzbuzz')
+    elif i%5 == 0:
         print('buzz')
+    elif i%3 == 0:
+        print('fizz')
     else: 
         print(f'{i}')
 
