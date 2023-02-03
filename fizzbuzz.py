e8303366ece9befa6da0ddcf8934cfa88edd97f4
johnmcha <johnnycha90@gmail.com>
2023-02-03 14:20:00 +0900

    feat: create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,8 @@
+# get integer from user
+user_num = int(input('hello there, enter a postive integer:'))
+# print user_num for debugging
+print(user_num)
+
+#iteration
+for i in range(1, user_num):
+    print(f'{i}')
